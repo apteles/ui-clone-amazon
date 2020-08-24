13ed738c3e199a7d7310a8d045c6b980c495cb2b
André Teles <31255210+apteles@users.noreply.github.com>
2020-08-24 17:14:03 -0300

    Update index.js
    update service firebase

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAP_6I6bAlFfCX6e6A1LxlB35gnurc5ZKk",
-  authDomain: "clone-719f3.firebaseapp.com",
-  databaseURL: "https://clone-719f3.firebaseio.com",
-  projectId: "clone-719f3",
-  storageBucket: "clone-719f3.appspot.com",
-  messagingSenderId: "274703371334",
-  appId: "1:274703371334:web:8bd701ee10f13a2aaa8e47",
+  apiKey: "your-key",
+  authDomain: "your-domain",
+  databaseURL: "your url",
+  projectId: "your project id",
+  storageBucket: "your bucket",
+  messagingSenderId: "messaging id",
+  appId: "your app id",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
